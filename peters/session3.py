e551094a1eca60e7fdac4fc4351041670af916ae
Christoph Peters <meinungen@gmx.de>
2013-02-05 11:56:55 +0100

    Session 3

---
+++ peters/session3.py
@@ -0,0 +1,28 @@
+import os
+class change_dir(object):
+	def __init__(self,dir_new):
+		self.dir_new = dir_new
+		self.dir_old = os.getcwd()
+		os.chdir(self.dir_new)
+	def __exit__(self):
+		os.chdir(self.dir_old)
+
+class CourseRepo(object):
+	def __init__(self, name):
+                self.surname = name
+	
+	@property
+	def surname(self):
+		return self._surname
+
+	@surname.setter
+	def surname(self,name):
+		self._surname=name
+		self.required = (".git","setup.py","README.md","scripts/getting_data.py","scripts/check_repo.py",self.surname + "/__init__.py",self.surname + "/session3.py") 
+
+	def check(self):
+		for entry in self.required:
+			if os.path.exists(entry) == False:
+				return False
+		return True
+		
---
+++ scripts/check_repo.py
@@ -0,0 +1,8 @@
+from peters.session3 import CourseRepo, change_dir
+import sys,os
+path = sys.argv[1]
+surname = path.split("/")[-1]
+cd = change_dir(path)
+cm = CourseRepo(surname)
+print cm.check()
+cd.__exit__()
