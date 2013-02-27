ed38ff68d3967afc1c2e5246c693dbb80499894b
Christoph Peters <meinungen@gmx.de>
2013-02-27 10:40:20 +0100

    profiler

--- scripts/check_repo.py
+++ scripts/check_repo.py
@@ -1,7 +1,13 @@
 from peters.session3 import CourseRepo, change_dir
 import sys,os
-path = sys.argv[1]
-surname = path.split("/")[-1]
-cd = change_dir(path)
-cm = CourseRepo(surname)
-print cm.check()
+
+@profile
+def main(argvs):
+	path = argvs[1]
+	surname = path.split("/")[-1]
+	cd = change_dir(path)
+	cm = CourseRepo(surname)
+	print cm.check()
+
+if __name__ == '__main__':
+    main(sys.argv)
--- scripts/getting_data.py
+++ scripts/getting_data.py
@@ -3,7 +3,12 @@
 from peters.session2 import read_data
 from peters.session2 import calc_status_repair 
 
+@profile
+def main():
+	doc = read_data()
+	iCounterRepair, iCounterAll = calc_status_repair(doc)
+	print str(iCounterRepair) + " out of " + str(iCounterAll) + " are under repair ("+ '%.1f' % (iCounterRepair/float(iCounterAll/100.0))  +"%)."
 
-doc = read_data()
-iCounterRepair, iCounterAll = calc_status_repair(doc)
-print str(iCounterRepair) + " out of " + str(iCounterAll) + " are under repair ("+ '%.1f' % (iCounterRepair/float(iCounterAll/100.0))  +"%)."
+if __name__ == '__main__':
+    #main(sys.argv)
+    main()
